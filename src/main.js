4bc12b25610a0069c4b3be283a58613d4bfe1b10
Анастасія Кошуцька <zaporozhetsnastya89@gmail.com>
2026-03-20 12:34:58 +0200

    update hw 12

--- src/main.js
+++ src/main.js
@@ -11,19 +11,18 @@ import {
 } from './js/render-functions.js';
 
 const refs = {
-  gallery: document.querySelector('.gallery'),
-  loader: document.querySelector('.loader'),
   userForm: document.querySelector('.form'),
   loadBtn: document.querySelector('.load-btn'),
 };
 
 let page = 1;
-let perPage = 15;
+let perPage;
 let totalHits;
 let userSearch = '';
 
 refs.userForm.addEventListener('submit', async e => {
   e.preventDefault();
+  hideLoadMoreButton();
   const formData = new FormData(e.target);
   userSearch = formData.get('search-text').trim();
   if (!userSearch) return;
@@ -35,6 +34,7 @@ refs.userForm.addEventListener('submit', async e => {
     page = 1;
     const data = await getImagesByQuery(userSearch, page);
     totalHits = data.totalHits;
+    perPage = data.hits.length;
     if (!data.hits.length) {
       iziToast.show({
         message: 'Sorry, there are no images matching your search query.',
@@ -45,7 +45,18 @@ refs.userForm.addEventListener('submit', async e => {
       return;
     }
     createGallery(data.hits);
-    showLoadMoreButton();
+
+    if (data.hits.length >= totalHits) {
+      hideLoadMoreButton();
+      iziToast.show({
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'topRight',
+        backgroundColor: '#33e917',
+        messageColor: 'white',
+      });
+    } else {
+      showLoadMoreButton();
+    }
   } catch (error) {
     iziToast.show({
       message: 'Something went wrong. Please try again later.',
@@ -61,7 +72,6 @@ refs.userForm.addEventListener('submit', async e => {
 refs.loadBtn.addEventListener('click', async e => {
   e.preventDefault();
   page += 1;
-  refs.gallery.appendChild(refs.loader);
   showLoader();
   hideLoadMoreButton();
   try {
@@ -85,6 +95,7 @@ refs.loadBtn.addEventListener('click', async e => {
         messageColor: 'white',
       });
     }
+    console.log(data);
   } catch (error) {
     iziToast.show({
       message: 'Something went wrong. Please try again later.',
