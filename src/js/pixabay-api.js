8acfa2315c5075553fbef1d43702b3e0e607e54c
Анастасія Кошуцька <zaporozhetsnastya89@gmail.com>
2026-03-20 11:43:31 +0200

    done hw-12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,14 +1,16 @@
 import axios from 'axios';
-export function getImagesByQuery(query) {
-  return axios
-    .get('https://pixabay.com/api/', {
-      params: {
-        key: '55008864-d5a1decafbeed91a0d2a4411c',
-        q: query,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: 'true',
-      },
-    })
-    .then(response => response.data);
+
+export async function getImagesByQuery(query, page) {
+  const res = await axios.get('https://pixabay.com/api/', {
+    params: {
+      key: '55008864-d5a1decafbeed91a0d2a4411c',
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      per_page: 100,
+      page: page,
+    },
+  });
+  return res.data;
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-
 import { getImagesByQuery } from './js/pixabay-api.js';
 import {
   createGallery,
@@ -10,30 +9,82 @@ import {
   showLoadMoreButton,
   hideLoadMoreButton,
 } from './js/render-functions.js';
-const userForm = document.querySelector('.form');
 
-userForm.addEventListener('submit', async e => {
+const refs = {
+  gallery: document.querySelector('.gallery'),
+  loader: document.querySelector('.loader'),
+  userForm: document.querySelector('.form'),
+  loadBtn: document.querySelector('.load-btn'),
+};
+
+let page = 1;
+let perPage = 100;
+let totalHits;
+let userSearch = '';
+
+refs.userForm.addEventListener('submit', async e => {
   e.preventDefault();
   const formData = new FormData(e.target);
-  const userSearch = formData.get('search-text').trim();
+  userSearch = formData.get('search-text').trim();
   if (!userSearch) return;
 
   clearGallery();
   showLoader();
-  showLoadMoreButton();
-  try {
-    const data = await getImagesByQuery(userSearch);
 
+  try {
+    page = 1;
+    const data = await getImagesByQuery(userSearch, page);
+    totalHits = data.totalHits;
     if (!data.hits.length) {
       iziToast.show({
         message: 'Sorry, there are no images matching your search query.',
         position: 'topRight',
-        backgroundColor: '#ef4040',
+        backgroundColor: '#e03710',
         messageColor: 'white',
       });
       return;
     }
     createGallery(data.hits);
+    showLoadMoreButton();
+  } catch (error) {
+    iziToast.show({
+      message: 'Something went wrong. Please try again later.',
+      position: 'topRight',
+      backgroundColor: '#ef4040',
+      messageColor: 'white',
+    });
+  } finally {
+    hideLoader();
+  }
+});
+
+refs.loadBtn.addEventListener('click', async e => {
+  e.preventDefault();
+  page += 1;
+  refs.gallery.appendChild(refs.loader);
+  showLoader();
+  hideLoadMoreButton();
+  try {
+    const data = await getImagesByQuery(userSearch, page);
+    createGallery(data.hits);
+    showLoadMoreButton();
+    const galleryCard = document.querySelector('.gallery-item');
+    if (galleryCard) {
+      const cardHeight = galleryCard.getBoundingClientRect().height;
+      window.scrollBy({
+        top: 2 * cardHeight,
+        behavior: 'smooth',
+      });
+    }
+    if (page * perPage >= totalHits) {
+      hideLoadMoreButton();
+      return iziToast.show({
+        position: 'topRight',
+        message: "We're sorry, there are no more posts to load",
+        backgroundColor: '#33e917',
+        messageColor: 'white',
+      });
+    }
   } catch (error) {
     iziToast.show({
       message: 'Something went wrong. Please try again later.',
